5dc74df68622f2e0b72b08a13860174738dc1fb2
tylerrimkunas <tyler.rimkunas@stonybrook.edu>
2021-10-13 19:42:29 -0400

    initial commit

---
+++ Push_files/Source/FPBasketBall/FPBasketBallGameModeBase.cpp
@@ -0,0 +1,18 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "FPBasketBallGameModeBase.h"
+
+
+void AFPBasketBallGameModeBase::StartPlay()
+{
+	Super::StartPlay();
+
+	check(GEngine != nullptr);
+
+	// Display a debug message for five seconds. 
+	// The -1 "Key" value argument prevents the message from being updated or refreshed.
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Hello World, this is FPSGameMode!"));
+
+}
+
